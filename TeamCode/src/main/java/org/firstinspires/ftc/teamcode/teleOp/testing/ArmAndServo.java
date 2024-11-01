0b3c50873437a5c36a81e3af055dfa2cae206b3b
Mehxeo <utsavd500@gmail.com>
2024-11-01 19:56:21 -0400

    Created arm and servo files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/testing/ArmAndServo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/testing/ArmAndServo.java
@@ -16,53 +16,48 @@ public class ArmAndServo extends OpMode {
     double current2 = Double.MAX_VALUE;
     double current3 = Double.MAX_VALUE;
     double current4 = Double.MAX_VALUE;
-    double current5 = Double.MAX_VALUE;
-
-    DcMotor outtakeMotor;
-    Servo armServo;
+    public DcMotor outtakeMotor;
+    public Servo armServo;
     @Override
     public void init() {
         telemetry.addData("Initialization","Starting...");
-        outtakeMotor = hardwareMap.get(DcMotor.class, "outtakeMotor");
-        armServo = hardwareMap.get(Servo.class, "armServo");
         telemetry.addData("Initialization","Done!");
+        outtakeMotor = hardwareMap.dcMotor.get("outtakeMotor");
+        armServo = hardwareMap.servo.get("");
     }
 
     @Override
     public void loop() {
-        if (gamepad1.x) {
 
+        double power = gamepad1.left_trigger - gamepad1.right_trigger;
+        outtakeMotor.setPower(power);
+        telemetry.addData("Power:", power);
+
+        if (gamepad1.x) { // Gamepad 1 x pressed -> Stop outtake motor and pull servo up
+            telemetry.addData("Gamepad 1", "x");
+            current1 = getRuntime();
+
+            servoRot = 1.0;
+            outtakeMotor.setPower(0);
+
+            armServo.setPosition(servoRot);
         }
-        if (g1_dpad_left){
-            launchServo.setPosition(-1);
 
-        }
-        if (g1_dpad_right){
-            launchServo.setPosition(1);
-
-        }
-
-        if (g2_a){
+        if (getRuntime() > current1 + .75){ // After .75 seconds from press -> Start outtake motor and shoot block out
             current2 = getRuntime();
-            clawPos = 1.0;
+            outtakeMotor.setPower(1);
+            current1 = Double.MAX_VALUE;
         }
 
-        if (getRuntime() > current2 + .75){
+        if (getRuntime() > current2 + .75){ // After 1.5 seconds from press -> Stop outtake motor and pull servo down
             current3 = getRuntime();
-            servoRot = 0.601;
+            outtakeMotor.setPower(0);
+            servoRot = 0.0;
+
+            armServo.setPosition(servoRot);
             current2 = Double.MAX_VALUE;
         }
 
-        if (getRuntime() > current3 + .75){
-            current4 = getRuntime();
-            clawPos = 0.0;
-            current3 = Double.MAX_VALUE;
-        }
-
-        if (getRuntime() > current4 + .75){
-            current5 = getRuntime();
-            servoRot = 0.0;
-            current4 = Double.MAX_VALUE;
-        }
+        telemetry.update();
     }
 }
