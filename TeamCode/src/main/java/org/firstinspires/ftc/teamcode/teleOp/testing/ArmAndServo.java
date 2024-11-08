5f4af11ba64aa24fbd5feb5dfb4c69a62bf489fd
Andrew Hu <andrewhu32009@gmail.com>
2024-11-07 20:54:16 -0500

    Andrew laptop buggin

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/testing/ArmAndServo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/testing/ArmAndServo.java
@@ -5,62 +5,119 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-import org.firstinspires.ftc.robotcore.external.Telemetry;
-
 @TeleOp(name="ArmAndServo")
 public class ArmAndServo extends OpMode {
 
-    final double outtakeSpeed = -1.0;
-    double servoRot = 0.0;
+    double outtakePower = -1.0;
+    double servoRot = 1.0;
+    double basketRot = 0.0;
+    boolean clicked = false;
+    boolean claw = false;
+    double clawRot = 0.0;
 
     //Automated time variables
     double current1 = Double.MAX_VALUE;
     double current2 = Double.MAX_VALUE;
     double current3 = Double.MAX_VALUE;
+    double current4 = Double.MAX_VALUE;
+    public DcMotor outtakeMotor;
+    public DcMotor vertLinearMotor;
+    public DcMotor horizLinearMotor;
 
-    DcMotor outtakeMotor;
-    Servo armServo;
+    public Servo armServo;
+    public Servo clawServo;
+    public Servo basketServo;
 
     @Override
     public void init() {
         telemetry.addData("Initialization","Starting...");
-        outtakeMotor = hardwareMap.get(DcMotor.class, "outtakeMotor");
-        armServo = hardwareMap.get(Servo.class, "armServo");
         telemetry.addData("Initialization","Done!");
-        telemetry.update();
+
+        outtakeMotor = hardwareMap.dcMotor.get("outtakeMotor"); // Motor Port 0
+        vertLinearMotor = hardwareMap.dcMotor.get("vertLinearMotor"); // Motor Port 1
+        horizLinearMotor = hardwareMap.dcMotor.get("horizLinearMotor"); // Motor Port 2
+
+        armServo = hardwareMap.servo.get("rotateServo"); // Servo Port 0
+        clawServo = hardwareMap.servo.get("clawServo"); // Servo Port 1
+        basketServo = hardwareMap.servo.get("basketServo"); // Servo Port 2
+
+        vertLinearMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
     @Override
     public void loop() {
-        // Gamepad 1 x pressed -> Stop outtake motor and pull servo up
-        if (gamepad1.x) {
-            telemetry.addData("Gamepad 1", "x pressed");
-            current1 = getRuntime();
-
-            servoRot = 1.0;
-            outtakeMotor.setPower(0);
-
-            armServo.setPosition(servoRot);
+        if (gamepad2.x){
+            clicked = true;
         }
 
-        // After .75 seconds from press -> Start outtake motor and shoot block out
-        if (getRuntime() > current1 + .75){
-            current2 = getRuntime();
-            outtakeMotor.setPower(1);
-            current1 = Double.MAX_VALUE;
+        if (clicked) {
+            // outtakeMotor (1 is outtake, -1 is intake)
+            // armServo (0 is up, 1 is down)
+            if (gamepad2.x) {
+                // Gamepad 1 x pressed -> Stop outtake motor and pull servo up
+                outtakePower = 0;
+                current1 = getRuntime();
+                servoRot = 0.0;
+            }
+
+            if (getRuntime() > current1 + .75){
+                // After .75 seconds from press -> Start outtake motor and shoot block out
+                current2 = getRuntime();
+                outtakePower = 1.0;
+                current1 = Double.MAX_VALUE;
+            }
+
+            if (getRuntime() > current2 + 1.5) {
+                // After 1.5 seconds from press -> Stop outtake motor and pull servo down
+                outtakePower = 0.0;
+                servoRot = 1.0;
+                current2 = Double.MAX_VALUE;
+                clicked = false;
+            }
+        }
+        else {
+            outtakePower = gamepad2.right_trigger - gamepad2.left_trigger;
         }
 
-        // After 1.5 seconds from press -> Stop outtake motor and pull servo down
-        if (getRuntime() > current2 + .75){
+        // Basket Drop Servo System
+        if (gamepad2.b) { // Open servo - drop the basket
             current3 = getRuntime();
-            outtakeMotor.setPower(0);
-            servoRot = 0.0;
-
-            armServo.setPosition(servoRot);
-            current2 = Double.MAX_VALUE;
+            basketRot = 1.0;
+        }
+        if (getRuntime() > current3 + 0.75) { // After .75 sec - bring the basket back
+            basketRot = 0.0;
+            current3 = Double.MAX_VALUE;
         }
 
-        telemetry.update();
-    }
+        // Claw Servo System - (0 is open, 1 is closed)
+        if (gamepad2.left_bumper){
+            clawRot = 0.0;
+        }
+        if (gamepad2.right_bumper){
+            clawRot = 1.0;
+        }
 
+        // Stop Linear Overextension & Over-retraction
+//        if (vertLinearMotor.getCurrentPosition() >= 1.0) {
+//
+//        }
+
+        outtakeMotor.setPower(outtakePower);
+
+        vertLinearMotor.setPower(gamepad2.right_stick_y);
+        horizLinearMotor.setPower(gamepad2.left_stick_y);
+
+        clawServo.setPosition(clawRot);
+        armServo.setPosition(servoRot);
+        basketServo.setPosition(basketRot);
+
+        telemetry.addData("Clicked", clicked);
+        telemetry.addData("Claw (open)", claw);
+        telemetry.addData("Runtime", getRuntime());
+        telemetry.addData("Current1, Current2, Current3", current1 + ", " + current2 + ", " + current3);
+        telemetry.addData("g2RStickY, g2LStickY", gamepad2.right_stick_y + ", " + gamepad2.left_stick_y);
+        telemetry.update();
+
+
+    }
 }
